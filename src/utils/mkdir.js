b7263efce4c4261511a14dea1fed2494830d21f0
d556f8 <l.juhyeonni@gmail.com>
2023-02-08 19:31:52 +0900

    FEAT: Added implementation

---
+++ src/utils/mkdir.js
@@ -0,0 +1,13 @@
+import fs from 'fs';
+
+export default function mkdir(location) {
+  fs.mkdir(location, { recursive: false }, (err) => {
+    if (err) {
+      if (err.code === 'EEXIST') {
+        console.log(`Directory already exists at ${location}`);
+      } else {
+        console.error(`An error occurred while creating the directory: ${err}`);
+      }
+    }
+  });
+}
