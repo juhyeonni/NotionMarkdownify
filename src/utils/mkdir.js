be03cdbc72cefb269a242e5db1eeaea763ff0321
d556f8 <l.juhyeonni@gmail.com>
2023-02-08 19:36:52 +0900

    UPDATE: Changed Log message

--- src/utils/mkdir.js
+++ src/utils/mkdir.js
@@ -4,7 +4,7 @@ export default function mkdir(location) {
   fs.mkdir(location, { recursive: false }, (err) => {
     if (err) {
       if (err.code === 'EEXIST') {
-        console.log(`Directory already exists at ${location}`);
+        console.log(`Directory already exists at ./${location}, continue to build`);
       } else {
         console.error(`An error occurred while creating the directory: ${err}`);
       }
